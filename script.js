9caab20482b7842cec07883aacd2873f56e70507
WillBeNothing <67840527+WillBeNothing@users.noreply.github.com>
2022-03-06 02:39:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ function Acessar() {
         const html = `
             <div id="Text">
                 <h1>The Death of Elisa Pacheco Bittercourt<br />
-                <span style="font-size:14px">A few&nbsp;months ago, the death of a girl called Elisa Pacheco, terrifying all the people around the city, but, what was more strage inthis case, was the fact that, there wasn&#39;t any evidence that could incriminate anyone. No clues how she died and who murdered a 16 yo girl.&nbsp;</span><br />
+                <span style="font-size:14px">A few&nbsp;months ago, the death of a girl called Elisa Pacheco, terrified all the people around the city, but, what was more strage in this case, was the fact that, there wasn&#39;t any evidence that could incriminate anyone. No clues how she died and who murdered a 16 yo girl.&nbsp;</span><br />
                 &nbsp;</h1>
 
-                <p>But I&#39;m here to turn this story. I know who murdered Elisa and how she died, but I&#39;m risking my life contacting you, so, I have to be discret. Find me, we have to talk better.&nbsp;</p>
+                <p>But I&#39;m here to turn this story. I know who murdered Elisa and how she died, but I&#39;m risking myself contacting you, so, I have to be discret. Find me, we have to talk better.&nbsp;</p>
 
                 <p>&nbsp;</p>
 
